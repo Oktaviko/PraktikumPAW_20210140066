e5caad0098a2295beb454ecf8bc2d02ac662e203
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:38:19 +0700

    Lanjutan Konsep Tipe Data

--- src/main/java/com/helloworld/helloworld/KonsepTipeData.java
+++ src/main/java/com/helloworld/helloworld/KonsepTipeData.java
@@ -3,5 +3,9 @@ package com.helloworld.helloworld;
 public class KonsepTipeData {
     public static void main(String[] args) {
         double iniDouble = 12.2424;
+
+        int decimalInt = 25;
+        int hexInt = 0xABCDEF;
+        int binInt = 0b1010101010101;
     }
 }
