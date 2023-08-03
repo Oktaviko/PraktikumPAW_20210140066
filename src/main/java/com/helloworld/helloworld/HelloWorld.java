550d241ca9c9365eedf3ab9d7c52504a5c027bae
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:35:01 +0700

    Program Hello World

---
+++ src/main/java/com/helloworld/helloworld/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.helloworld.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Program Hello World
+        System.out.println("Hello World");
+    }
+}
