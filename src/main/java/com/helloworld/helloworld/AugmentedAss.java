1d758a5de9179edcb3ba2cd9a058b7625ae660b9
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:47:48 +0700

    Augmented Assignments

---
+++ src/main/java/com/helloworld/helloworld/AugmentedAss.java
@@ -0,0 +1,18 @@
+package com.helloworld.helloworld;
+
+public class AugmentedAss {
+    public static void main(String[] args) {
+        System.out.println("\n");
+                System.out.println("Augmented Assignment");
+                int c = 100;
+
+                c += 10;
+                System.out.println(c);
+
+                c -= 10;
+                System.out.println(c);
+
+                c *= 10;
+                System.out.println(c);
+    }
+}
