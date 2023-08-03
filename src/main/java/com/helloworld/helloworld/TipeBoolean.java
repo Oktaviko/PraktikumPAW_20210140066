1fbb2eccb8e7f5c172967f109ffcda11bf3f90d3
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:39:46 +0700

    Tipe Data Boolean

---
+++ src/main/java/com/helloworld/helloworld/TipeBoolean.java
@@ -0,0 +1,13 @@
+package com.helloworld.helloworld;
+
+public class TipeBoolean {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Boolean");
+        boolean benar = true;
+        boolean salah = false;
+
+        System.out.println(benar);
+        System.out.println(salah);
+    }
+}
