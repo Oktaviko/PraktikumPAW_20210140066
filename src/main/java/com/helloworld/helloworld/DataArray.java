616cb119549bf930f5bd8add1c5e5c5d341dd179
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:44:21 +0700

    Tipe Data Array

---
+++ src/main/java/com/helloworld/helloworld/DataArray.java
@@ -0,0 +1,21 @@
+package com.helloworld.helloworld;
+
+public class DataArray {
+    public static void main(String[] args) {
+        String[] arrayString;
+                arrayString = new String[3];
+        arrayString[0] = "Okta";
+        arrayString[0] = "Alfi";
+        arrayString[0] = "Pratama";
+
+        int[] arrayInt = new int[] {
+                        10, 90, 80, 67, 29
+        };
+
+        long[] arraylong = {
+                        10, 90, 80, 67, 29
+        };
+
+        arraylong[0] = 100;
+    }
+}
