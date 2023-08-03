3a1666adf12bae40ee70dae72e9856f44be1b857
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:40:49 +0700

    Membenarkan VarKey revisi 2

--- src/main/java/com/helloworld/helloworld/Varkey.java
+++ src/main/java/com/helloworld/helloworld/Varkey.java
@@ -4,6 +4,8 @@ public class Varkey {
    public static void main(String[] args) {
      int age = 30;
     String address = "Indonesia";
+    String name;
+    name ="Oktaviko Rizki Pratama";
 
     System.out.println(name);
     System.out.println(age);
