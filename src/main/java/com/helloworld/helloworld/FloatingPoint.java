3e04fe61a58794050d6838e2e247d492ba690b3f
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:37:02 +0700

    Floating Point Number

---
+++ src/main/java/com/helloworld/helloworld/FloatingPoint.java
@@ -0,0 +1,8 @@
+package com.helloworld.helloworld;
+
+public class FloatingPoint {
+    public static void main(String[] args) {
+        float iniFloat = 10.12F;
+        long iniLong4 = (long) iniFloat;
+    }
+}
