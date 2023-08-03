d6d7536ecceb6112e680fa6cb5f02afe29b7b98c
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:46:53 +0700

    Operasi Matematika

---
+++ src/main/java/com/helloworld/helloworld/Operasimath.java
@@ -0,0 +1,16 @@
+package com.helloworld.helloworld;
+
+public class Operasimath {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Operasi Matematika");
+        int a = 100;
+        int b = 10;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+    }
+}
