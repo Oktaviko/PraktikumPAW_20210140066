34403f14259758c9a78c7a7ccaa4d99422f13331
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:52:17 +0700

    Expression

---
+++ src/main/java/com/helloworld/helloworld/Expressions.java
@@ -0,0 +1,24 @@
+package com.helloworld.helloworld;
+import java.util.Date;
+
+public class Expressions {
+    public static void main(String[] args) {
+         // Expression
+         int value;
+
+         value = 10;
+ 
+         System.out.println(value = 100);
+ 
+         // assignment statement
+         double aValue = 8933.234;
+         // increment statement
+         aValue++;
+         // method invocation statement
+         System.out.println("Hello World!");
+         // object creation statement
+         Date date = new Date();
+ 
+         System.out.println(date);
+    }
+}
