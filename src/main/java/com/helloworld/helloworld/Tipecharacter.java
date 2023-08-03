138e757ca6f72afea7264c57d9661432ca55a482
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:39:10 +0700

    Tipe Data Character

---
+++ src/main/java/com/helloworld/helloworld/Tipecharacter.java
@@ -0,0 +1,20 @@
+package com.helloworld.helloworld;
+
+public class Tipecharacter {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Tipe data char");
+        char k = 'K';
+        char e = 'E';
+        char o = 'O';
+
+        System.out.println("Tipe data di java");
+        System.out.print(e);
+        System.out.print(k);
+        System.out.println(o);
+
+        System.out.println(e);
+        System.out.println(k);
+        System.out.println(o);
+    }
+}
