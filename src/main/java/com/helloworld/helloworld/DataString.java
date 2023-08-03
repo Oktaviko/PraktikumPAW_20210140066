737dbc00c7355aa7278bc6cccea5f0f205880091
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:41:49 +0700

    Tipe Data String

---
+++ src/main/java/com/helloworld/helloworld/DataString.java
@@ -0,0 +1,15 @@
+package com.helloworld.helloworld;
+
+public class DataString {
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Tipe data String");
+        String firstname = "Farhan Taufiqul";
+        String lastname = "Ihsan";
+        String fullname = firstname + " " + lastname;
+
+        System.out.println(firstname);
+        System.out.println(lastname);
+        System.out.println(fullname);
+    }
+}
