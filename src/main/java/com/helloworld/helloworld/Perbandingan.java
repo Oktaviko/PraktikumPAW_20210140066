4f276e521399b90eefbe116df1a1034d63344103
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:54:19 +0700

    Operasi Perbandingan

---
+++ src/main/java/com/helloworld/helloworld/Perbandingan.java
@@ -0,0 +1,16 @@
+package com.helloworld.helloworld;
+
+public class Perbandingan {
+    public static void main(String[] args) {
+        // Operasi Perbandingan
+        int value1 = 100;
+        int value2 = 100;
+
+        System.out.println(value1 > value2);
+        System.out.println(value1 < value2);
+        System.out.println(value1 >= value2);
+        System.out.println(value1 <= value2);
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value2);
+    }
+}
