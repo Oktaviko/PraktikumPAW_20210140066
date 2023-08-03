178224c5ecff4b2406644db0c22f1160bc0b5d20
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 18:35:59 +0700

    Tipe Data Number

---
+++ src/main/java/com/helloworld/helloworld/TipeDataNumber.java
@@ -0,0 +1,18 @@
+package com.helloworld.helloworld;
+
+public class TipeDataNumber {
+    public static void main(String[] args) {
+        byte iniByte = 100;
+        short iniShort = 1000;
+        int iniInt = 1000000;
+        long iniLong = 1000000l;
+        long iniLong2 = 1000000l;
+        byte iniByte2 = 12;
+        short iniShort_Konversi = iniByte;
+        short iniShort2 = 32_767;
+        int iniInt2 = 2_147_483_647;
+        long iniLong3 = 923_337_203l;
+
+    }
+    
+}
