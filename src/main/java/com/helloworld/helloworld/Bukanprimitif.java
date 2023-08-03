19e143491ff11824508fadb9ffc2d8c1ddb0c08f
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:43:09 +0700

    Tipe Data Bukan Primitif

---
+++ src/main/java/com/helloworld/helloworld/Bukanprimitif.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class Bukanprimitif {
+    
+    public static void main(String[] args) {
+        System.out.println("\n");
+        System.out.println("Array");
+        String[] arrayString;
+        arrayString = new String[3];
+    }
+}
