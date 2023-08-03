339e08a4a18e06ea9ffcdaf8118dfd1c7dba03ea
Oktaviko Rizki <oktaviko.rizki.ft21@mail.umy.ac.id>
2023-08-03 19:41:48 +0700

    Kata Kunci Final

---
+++ src/main/java/com/helloworld/helloworld/KunciFial.java
@@ -0,0 +1,11 @@
+package com.helloworld.helloworld;
+
+public class KunciFial {
+    public static void main(String[] args) {
+        System.out.println("\n");
+                System.out.println("Final");
+                final String name1 = "Oktaviko Rizki Pratama";
+                int age1 = 30;
+                String address1 = "Indonesia";
+    }
+}
